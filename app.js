ec6212c630e5e3b840ef4453d5c4402dc6cc47cf
RubenManurung <rubenmichael199@gmail.com>
2020-11-10 13:20:28 +0700

    add Procfile

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ app.post("/", function(req, res){
   });
 });
 
-app.listen(3000, ()=>{
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, ()=>{
   console.log("Server is running in port 3000");
 });
