40e69e49992d3ce6d48a4975f651a72dfcbf4968
RubenManurung <rubenmichael199@gmail.com>
2020-11-10 09:01:54 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const ejs = require("ejs");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/index.html")
+
+});
+
+
+app.post("/", function(req, res){
+  const cityName = req.body.cityName;
+  const apiKey = "81d7037be4c6c0a13de7b9bbfc9ee139";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey+"&lang=id&units=metric";
+
+  https.get(url, function(response){
+    console.log("Status code : "+response.statusCode);
+    if(response.statusCode == 200){
+      response.on("data", function(data){
+        const weatherApp = JSON.parse(data);
+        const temp = weatherApp.main.temp;
+        const weatherDesc = weatherApp.weather[0].description;
+        const iconCode = weatherApp.weather[0].icon;
+        const imgIcon = "http://openweathermap.org/img/wn/"+iconCode+"@2x.png";
+
+        res.render("weather", {
+          cityName: cityName,
+          temp: temp,
+          weatherDesc: weatherDesc,
+          imgIcon: imgIcon
+        });
+    });
+    }else{
+      res.render("error");
+    }
+      
+  });
+});
+
+
+//belum implementasi jika nama kota salah dari client side
+
+
+
+
+
+app.listen(3000, ()=>{
+  console.log("Server is running in port 3000");
+});
